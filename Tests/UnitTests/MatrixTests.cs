cc4d1de3e665e2975ce1f0a313b5d232ceb22ed1
Łukasz Adach <lukasz.adach7@gmail.com>
2018-04-16 23:37:26 +0200

    struggling with tests

---
+++ Tests/UnitTests/MatrixTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NeuralNetworkScratch;
+
+namespace Tests.UnitTests
+{
+
+        [TestFixture]
+        public class MatrixTests
+        {
+
+            [Test]
+            public void MatrixSumTest()
+            {
+                double[,] m = new double[2, 2] { { 1, 2 }, { 4, 5 } };
+                double result = m.Sum();
+                Assert.AreNotEqual(12, result);
+            }
+
+
+
+        }
+    
+}
